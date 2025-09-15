da32a881c443786a06bd2d7da6ecf80ff65c8fdd
thomaslucking <telucking@gmail.com>
2025-09-15 11:48:36 +0200

    feat(strings): Finished strings manipulation

--- src/basics/strings.js
+++ src/basics/strings.js
@@ -8,4 +8,23 @@
  */
 export function findAndReplacePreservingCase(needle, haystack, newWord) {
   // Write your code here
+  if (
+    typeof needle !== 'string' ||
+    typeof haystack !== 'string' ||
+    typeof newWord !== 'string'
+  ) {
+    throw new TypeError('Invalid input')
+  }
+  const regex = new RegExp(needle, 'gi')
+  // this processes each match found in the regex.
+  return haystack.replaceAll(regex, (match) => {
+    let answer = '' // a variable to store the result
+    for (let i = 0; i < match.length && i < newWord.length; i++) { // this loops over the letters of match and newWord
+      answer += // this adds newword to the answer and it checks if the match regex is uppercase or lowercase
+        match[i] === match[i].toUpperCase()
+          ? newWord[i].toUpperCase() // if the matched letter is uppercase, use uppcase for newWord
+          : newWord[i].toLowerCase() // vise versa
+    } 
+    return answer // return the processed replacement.
+  })
 }
