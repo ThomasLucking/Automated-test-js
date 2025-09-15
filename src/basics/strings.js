58f7b2bb3c6ecbd3233b19c4873a2c23fe6f0888
thomaslucking <telucking@gmail.com>
2025-09-15 11:57:55 +0200

    fix(formatting) fixed formating for arrays and strings

--- src/basics/strings.js
+++ src/basics/strings.js
@@ -15,16 +15,17 @@ export function findAndReplacePreservingCase(needle, haystack, newWord) {
   ) {
     throw new TypeError('Invalid input')
   }
-  const regex = new RegExp(needle, 'gi') // creates a new regex with need and the global and case-insensitive 
+  const regex = new RegExp(needle, 'gi') // creates a new regex with need and the global and case-insensitive
   // this processes each match found in the regex.
   return haystack.replaceAll(regex, (match) => {
     let answer = '' // a variable to store the result
-    for (let i = 0; i < match.length && i < newWord.length; i++) { // this loops over the letters of match and newWord
+    for (let i = 0; i < match.length && i < newWord.length; i++) {
+      // this loops over the letters of match and newWord
       answer += // this adds newword to the answer and it checks if the match regex is uppercase or lowercase
         match[i] === match[i].toUpperCase()
           ? newWord[i].toUpperCase() // if the matched letter is uppercase, use uppcase for newWord
           : newWord[i].toLowerCase() // vise versa
-    } 
+    }
     return answer // return the processed replacement.
   })
 }
